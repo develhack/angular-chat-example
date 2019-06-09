c7fe8f4e216debde74ffdd843e8322d7b6c2155b
Yosuke Otsuka <yosuke.otsuka@develhack.com>
2019-06-09 11:58:55 +0900

    ルーティングの設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInTo(['chat'])) },
+  { path: 'chat', component: ChatComponent, ...canActivate(redirectUnauthorizedTo(['login'])) },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
